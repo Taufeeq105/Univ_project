119e5df6742473c62e17d26dd0c93e1108983ebd
Taufeeq105 <108462349+Taufeeq105@users.noreply.github.com>
2022-12-03 00:39:28 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -6,17 +6,8 @@ st.title('DE Lab Dashboard')
 
 
 @st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-
-st.subheader('Number of pickups by hour')
-option = st.selectbox(
-    'How would you like to be contacted?',
+option = st.sidebar.selectbox(
+    'Select Query you want to run?',
     ('Q1', 'Q2', 'Q3','Q4','Q5'))
 
 st.write('You selected:', option)
