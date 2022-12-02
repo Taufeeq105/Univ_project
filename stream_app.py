e5031ea471b110fd98ec2112ee3b3bb4d88deffa
Taufeeq105 <108462349+Taufeeq105@users.noreply.github.com>
2022-12-03 00:33:50 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -2,11 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('DE Lab Dashboard')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
 def load_data(nrows):
@@ -16,21 +13,10 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Q1', 'Q2', 'Q3','Q4','Q5'))
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+st.write('You selected:', option)
