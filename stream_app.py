fda25ca246c87f63503f7491ae8cbe6377fde11f
Taufeeq105 <108462349+Taufeeq105@users.noreply.github.com>
2022-12-03 00:54:38 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -3,11 +3,8 @@ import pandas as pd
 import numpy as np
 
 st.title('DE Lab Dashboard')
-
+ch=['query1','query2','query3','query4']
+sel=st.sidebar.selectbox("choose query",ch)
 
 @st.cache
-option = st.sidebar.selectbox(
-    'Select Query you want to run?',
-    ('Q1', 'Q2', 'Q3','Q4','Q5'))
 
-st.write('You selected:', option)
