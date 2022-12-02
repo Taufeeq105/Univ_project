8472f64afd1e558fd2262191077a28ba3204a213
Taufeeq105 <108462349+Taufeeq105@users.noreply.github.com>
2022-12-03 00:55:24 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+@st.cache
 st.title('DE Lab Dashboard')
 ch=['query1','query2','query3','query4']
 sel=st.sidebar.selectbox("choose query",ch)
-
-@st.cache
-
