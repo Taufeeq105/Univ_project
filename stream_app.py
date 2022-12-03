b4992236c171c72cfccb92f7792df340c0b71ead
Taufeeq105 <108462349+Taufeeq105@users.noreply.github.com>
2022-12-03 22:06:13 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 st.title('DE Lab Dashboard')
 ch=['query1','query2','query3','query4']
 sel=st.sidebar.selectbox("choose query",ch)
-
+a1=[i for i in range(20)]
+a2=[j+5 for j in range(20)]
 data = {
-  "calories": [i for i in range(20)],
-  "duration": [j+5 for j in range(20)]
+  "calories":a1,
+  "duration":a2
 }
 df = pd.DataFrame(data)
 
