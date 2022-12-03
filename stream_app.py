44ca21b64103490f261b47cd0e7ed28ef902be35
Taufeeq105 <108462349+Taufeeq105@users.noreply.github.com>
2022-12-03 22:03:55 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,7 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 st.title('DE Lab Dashboard')
 ch=['query1','query2','query3','query4']
 sel=st.sidebar.selectbox("choose query",ch)
+
+data = {
+  "calories": [i for i in range(20)],
+  "duration": [j+5 for j in range(20)]
+}
+df = pd.DataFrame(data)
+
+if sel=='query1':
+  fig, ax = plt.subplots()
+  ax.hist(df['duration'])
+  st.pyplot(fig)
+  
+if sel=='query2':
+  fig, ax = plt.subplots()
+  ax.hist(df['calories'])
+  st.pyplot(fig)
+  
