507767992e130e7567687ec8b33d1624b847d177
Taufeeq105 <108462349+Taufeeq105@users.noreply.github.com>
2022-12-03 22:06:43 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -9,10 +9,7 @@ ch=['query1','query2','query3','query4']
 sel=st.sidebar.selectbox("choose query",ch)
 a1=[i for i in range(20)]
 a2=[j+5 for j in range(20)]
-data = {
-  "calories":a1,
-  "duration":a2
-}
+data = {"calories":a1,"duration":a2}
 df = pd.DataFrame(data)
 
 if sel=='query1':
